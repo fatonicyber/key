bf2aa6f36d9ede14ac1f7039293392a4f9e1affe
fatonicyber <54709315+fatonicyber@users.noreply.github.com>
2019-09-11 21:52:01 +0700

    Create mr.black.py

---
+++ mr.black.py
@@ -0,0 +1,36 @@
+import os
+from time import sleep
+
+
+a ='\033[92m'
+b ='\033[91m'
+c ='\033[0m'
+os.system('clear')
+print(a+'\t  hallo agan agan')
+print(b+'\t   mr.black0304')
+print('\t wa:085601015635')
+print(a+'+'*40)
+print('\nProses..')
+sleep(1)
+print(b+'\n[!] sabar ya sayang mwehehe..')
+sleep(1)
+try:
+      os.mkdir('/data/data/com.termux/files/home/.termux')
+except:
+      pass
+print(a+'[!]Success !')
+sleep(1)
+print(b+'\n[!] Making setup file..')
+sleep(1)
+
+key = "extra-keys = [['ESC','/','-','HOME','UP','END','PGUP'],['TAB','CTRL','ALT','LEFT','DOWN','RIGHT','PGDN']]"
+kontol = open('/data/data/com.termux/files/home/.termux/termux.properties','w')
+kontol.write(key)
+kontol.close()
+sleep(1)
+print(a+'[!] Success !')
+sleep(1)
+print(b+'\n[!] Setting up..')
+sleep(2)
+os.system('termux-reload-settings')
+print(a+'[!] Successfully !! ^^'+c+'\n\nhubungi 085691015635 untuk requests\natau pertanyaan, atau hubungi mhmdfatoni0304@gmail.com\nThanks :*\n\n')
